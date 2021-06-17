71f4052de9dc2b61909fd1fb471e7baf703b9d59
wvovaw <vladimir5313375015@gmail.com>
2021-06-17 19:53:50 +0500

    Add comments section

--- src/layouts/Slavery.vue
+++ src/layouts/Slavery.vue
@@ -1,40 +1,38 @@
 <template>
-  <div id="container">
-    <div id="slavery">
-      <h3>I've been drafted into the russian army ☮</h3>
-      <p>
-        will be home in
-          <flip-countdown deadline="2022-06-22 00:00:00"></flip-countdown>
-      </p>
-    </div>
+  <div id="slavery">
+    <hr />
+    <h3>I've been drafted into the russian army ☮</h3>
+    <p>will be home in</p>
+    <Countdown :date="deadline" @onFinish="finish()"></Countdown>
     <div id="video">
-      <youtube
-        :player-vars="playerVars"
-        ref="youtube"
-        @playing="playing"
-      />
+      <youtube :player-vars="playerVars" ref="youtube" @playing="playing" />
     </div>
+    <hr />
+    <h3 id="comments">
+      Leave a memes you'd like to share with me right now. IT IS NECESSARY!
+    </h3 >
+    <Disqus :shortname="shortname" />
+    <hr />
+    <h6>Ave Gestald!</h6>
   </div>
 </template>
 
 <script>
 import VueYoutube from "vue-youtube";
-// import FlipCountdown from "vue2-flip-countdown";
-if (process.isClient) {
-  const FlipCountdown = require("vue2-flip-countdown");
-  Vue.use(FlipCountdown);
-  this.components.FlipCountdown = FlipCountdown;
-}
+import Countdown from "../components/Countdown";
+import { Disqus } from "vue-disqus";
 
 export default {
-  components: { VueYoutube },
+  components: { Countdown, VueYoutube, Disqus },
   data() {
     return {
+      deadline: new Date("2022-06-22T16:37:00"),
+      shortname: "https-wvovaw-github-io",
       playerVars: {
-        playlist: 'WM8bTdBs-cw,q7DfQMPmJRI,5ClCaPmAA7s,1xp6DOifzYo,hxsld16TjSU,JIm5ysUXVok,PcSoLwFisaw,rj3IbZV_YQM,POdNTtDi-eA,9D2R69gVyZ0,brGNqP1EBLE,N26N7lQHNW8,ed_UWFr13pU,BNTo34xOwoM,qJq9y9xPKWs,civuoU_NE38,M8r5rnamdaI,vvCM7Qig9qI,qnlhVVwBfew,LMCU-lYb0G4,bndL7wwAj0U,flRx55ucBlQ,3BXyEUOuNds,75hGl9stdQI,rDrhI1GUnNg,cDuLYGYHhQk,MM62wjLrgmA,-_nQhGR0K8M,q0BOpkA2Vs4,Y7JG63IuaWs,dUQjGpm5Kr8,4MzVuHqsNoM,JAzLQ5aeAOw,7RmLkY1387A,NfpwKs1REg0,1Iz5DaX8f7w,yat1iwCkgD8,wbAanI0Q68w,gBnalcSi138,O98mpBWsab8,QOR0RilgO2w,StwAtxgDUDc,Oko1aqm3K1o,ImQmta2XYqo,9cwrLM3pDeY,baiQKqfqOT0,tqjOrkMHUkk,-T2cniIZ3sM,O5Ohwdps7u4,Ebr85ORhciU,CeTKHfMNO58,DhMv5WVOli4,7w4AE4Mat8o,Cw8V1TQXcGc,2vFY8ufrslw,QixM_mwdVoQ,dzEahGFoEsw,FgNuLP3xpjo,b2BKKeiYH24,EMESjt71SYs,xH9Y3qEHAuw,hCYOV60WWW4,qEmfBRF0Mg0,matrK_WTAkQ,kpdHuo7YwBU,E3wIAf6ai_0,6JLF0GAdJAo',
+        playlist:
+          "WM8bTdBs-cw,q7DfQMPmJRI,5ClCaPmAA7s,1xp6DOifzYo,hxsld16TjSU,JIm5ysUXVok,PcSoLwFisaw,rj3IbZV_YQM,POdNTtDi-eA,9D2R69gVyZ0,brGNqP1EBLE,N26N7lQHNW8,ed_UWFr13pU,BNTo34xOwoM,qJq9y9xPKWs,civuoU_NE38,M8r5rnamdaI,vvCM7Qig9qI,qnlhVVwBfew,LMCU-lYb0G4,bndL7wwAj0U,flRx55ucBlQ,3BXyEUOuNds,75hGl9stdQI,rDrhI1GUnNg,cDuLYGYHhQk,MM62wjLrgmA,-_nQhGR0K8M,q0BOpkA2Vs4,Y7JG63IuaWs,dUQjGpm5Kr8,4MzVuHqsNoM,JAzLQ5aeAOw,7RmLkY1387A,NfpwKs1REg0,1Iz5DaX8f7w,yat1iwCkgD8,wbAanI0Q68w,gBnalcSi138,O98mpBWsab8,QOR0RilgO2w,StwAtxgDUDc,Oko1aqm3K1o,ImQmta2XYqo,9cwrLM3pDeY,baiQKqfqOT0,tqjOrkMHUkk,-T2cniIZ3sM,O5Ohwdps7u4,Ebr85ORhciU,CeTKHfMNO58,DhMv5WVOli4,7w4AE4Mat8o,Cw8V1TQXcGc,2vFY8ufrslw,QixM_mwdVoQ,dzEahGFoEsw,FgNuLP3xpjo,b2BKKeiYH24,EMESjt71SYs,xH9Y3qEHAuw,hCYOV60WWW4,qEmfBRF0Mg0,matrK_WTAkQ,kpdHuo7YwBU,E3wIAf6ai_0,6JLF0GAdJAo",
         autoplay: 1,
         origin: "https://wvovaw.github.io",
-        // origin: "http://localhost:8080",
       },
     };
   },
@@ -47,12 +45,14 @@ export default {
       this.$youtube.PlaylistItems();
     },
   },
-  mounted: async function () {
-  },
+  mounted: async function () {},
   methods: {
     playing() {
       this.player.setVolume(10);
       this.player.playVideo();
+    },
+    finish() {
+      console.log("aboba");
     },
   },
 };
@@ -60,6 +60,7 @@ export default {
 
 <style>
 #slavery {
+  text-align: center;
   padding: 1% 15% 1% 15%;
   background-color: var(--background);
   color: var(--foreground);
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,13 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Wvovaw\'s personal page'
+    title: 'Wvovaw\'s personal page',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'description', content: 'Wvovaw\'s personal website' },
+      { property: 'og:url', content: 'https://wvovaw.github.io'},
+      { property: 'og:site_name', content: 'wvovaw'},
+    ]
   }
 }
 </script>
